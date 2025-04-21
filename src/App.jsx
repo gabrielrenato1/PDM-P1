2e956bb8975c0eef26064165d4c688806bf2ab3e
Felipe Lancho <felipemarin46@gmail.com>
2025-04-21 16:50:07 -0300

    feat(p1): exibe localidades reais

--- src/App.jsx
+++ src/App.jsx
@@ -4,53 +4,10 @@ import LocalidadeLista from "./components/LocalidadeLista"
 import ViaCepClient from "./utils/ViaCepClient"
 
 class App extends Component {
-  localidades = [
-    {
-        "cep": "01001-000",
-        "logradouro": "Praça da Sé",
-        "complemento": "lado ímpar",
-        "unidade": "",
-        "bairro": "Sé",
-        "localidade": "São Paulo",
-        "uf": "SP",
-        "estado": "São Paulo",
-        "regiao": "Sudeste",
-        "ibge": "3550308",
-        "gia": "1004",
-        "ddd": "11",
-        "siafi": "7107"
-      },
-      {
-        "cep": "01444-003",
-        "logradouro": "Praça do Sol",
-        "complemento": "lado ímpar",
-        "unidade": "",
-        "bairro": "Sol",
-        "localidade": "Rio de Janeiro",
-        "uf": "RJ",
-        "estado": "Rio de Janeiro",
-        "regiao": "Sudeste",
-        "ibge": "3550308",
-        "gia": "1004",
-        "ddd": "11",
-        "siafi": "7107"
-      },
-      {
-        "cep": "03414-023",
-        "logradouro": "Lago das Flores",
-        "complemento": "lado ímpar",
-        "unidade": "",
-        "bairro": "Santiago",
-        "localidade": "Minas Gerais",
-        "uf": "MG",
-        "estado": "Minas Gerais",
-        "regiao": "Sudeste",
-        "ibge": "3550308",
-        "gia": "1004",
-        "ddd": "11",
-        "siafi": "7107"
-      }
-  ];
+  
+  state = {
+    localidades: []
+  }
 
   buscarCep = (inputCep) => {
 
@@ -63,7 +20,10 @@ class App extends Component {
         if(result.data.erro){
           alert("Erro ao consultar o CEP: " + inputCep)
         }else{
-          console.log(result.data)
+          this.setState(prevState => ({
+            localidades: [result.data, ...prevState.localidades]
+          }));
+           
         }
 
       }).catch(error => {
@@ -87,7 +47,7 @@ class App extends Component {
             <Busca buscarCep={this.buscarCep} dica='Digite um CEP'/>
           </div>
           <div>
-            <LocalidadeLista localidades={this.localidades}/>
+            <LocalidadeLista localidades={this.state.localidades}/>
           </div>
       </div>  
     )
